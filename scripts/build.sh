52575fe719e7c9ed0d63b96a4a4798d103fe98cb
Ravi Yenduri <ravi.yenduri@climate.com>
2015-11-11 13:17:49 -0600

    Force to build a static plugin

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,6 @@
 set -ex
 
 ./autogen.sh
-./configure CXXFLAGS=-I/usr/local/include LDFLAGS=-L/usr/local/lib
+./configure CXXFLAGS=-I/usr/local/include LDFLAGS="/usr/local/lib/libprotobuf.a /usr/local/lib/libprotoc.a -L/usr/local/lib"
 make clean
 make -j8 && make install
